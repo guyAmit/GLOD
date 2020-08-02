899312f7aa9962d1aa750761c005bae9c4523ae6
guy <guy5@post.bgu.ac.il>
2020-08-02 12:35:06 +0300

    Docstring and bug

--- GLOD/utils/ood_utils.py
+++ GLOD/utils/ood_utils.py
@@ -3,6 +3,20 @@ from sklearn.metrics import auc, precision_recall_curve, roc_auc_score
 
 
 def search_thers(preds_in, level):
+    '''search_thresh(preds_in, level)
+    search the threshold for a spesific tnr level
+    Parameters
+    ----------------
+    'preds_in': Tensor
+        thensor containing the predictions for in distribution datasets
+
+    'level': float
+        tnr level e.g. 0.95, 0.99
+
+    Return
+    -----------------
+    threshold value, such that (preds_in <= val).sum() <=level
+    '''
     step = 1
     val = -25
     eps = 0.0005
@@ -20,6 +34,23 @@ def search_thers(preds_in, level):
 
 
 def detection_accuracy(start, end, preds_in, preds_ood):
+    '''detection_accuracy(start, end, preds_in, preds_ood)
+    search for the threshold which will yeilds in the maximum detection
+    accuracy
+
+    Parameters
+    ----------------
+    'start, end': floats
+        floats values that represent the start, end of the predictions range
+    'preds_in, preds_ood': Tensor
+        Tensors representing the in distribution and the out distribution predictions
+
+    Return
+    -----------------
+    threshold value that maxmize the detection accuracy
+
+    the detection accuracy
+    '''
     step = (end-start)/10000
     val = start
     max_det = 0
@@ -36,13 +67,39 @@ def detection_accuracy(start, end, preds_in, preds_ood):
 
 
 def auroc_score(preds_in, preds_ood):
+    '''auroc_score(preds_in, preds_ood)
+    calculate the aucroc score of the detector
+
+    Parameters
+    ----------------
+    'preds_in, preds_ood': Tensor
+        Tensors representing the in distribution and the out distribution
+        predictions
+
+    Return
+    -----------------
+    the aucroc score
+    '''
     y_true = np.concatenate((np.zeros(preds_ood.size(0)),
                              np.ones(preds_in.size(0))))
     preds = np.concatenate((preds_ood.numpy(), preds_in.numpy()))
     return roc_auc_score(y_true, preds)
 
 
-def aurpr_score(preds_in, preds_ood):
+def aupr_score(preds_in, preds_ood):
+    '''aupr_score(preds_in, preds_ood)
+    calculate the area under precision recall curve of the detector predictions
+
+    Parameters
+    ----------------
+    'preds_in, preds_ood': Tensor
+        Tensors representing the in distribution and the out distribution
+        predictions
+
+    Return
+    -----------------
+    the aucroc score
+    '''
     y_true = np.concatenate((np.zeros(preds_ood.size(0)),
                              np.ones(preds_in.size(0))))
     preds = np.concatenate((preds_ood.numpy(), preds_in.numpy()))
